a22cf91a083fe7196c4a9503401b4e3c5718d8e2
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-07-10 11:43:14 +0530

    remove last

--- basics.java
+++ basics.java
@@ -1,117 +1,136 @@
-public class basics{
+public class basics {
     public static void main(String[] args) {
-        Linked list=new Linked();
+        Linked list = new Linked();
 
-        //System.out.println(list.size);
-
-        list.addFirst(10);
-        list.addFirst(20);
-        list.addFirst(30);
-
-        list.addLast(40);
-        list.addLast(50);
-
-        // list.addAt(900, 2);
-        // list.addAt(700, 3);
-
-        list.removeFirst();
-        list.removeFirst();
-
-        list.display();
-
+       list.addLast(30);
+       list.addLast(40);
+       list.addLast(50);
+      
+        System.out.println(list.removeLast());
+       list.display();
     }
 }
 
-class Node{
-    int data ;
+class Node {
+    int data;
     Node next;
 
-    Node(int data){
-        this.data=data;
-        this.next=null;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     }
 }
 
-class Linked{
-    Node head ;
-    Node tail ;
+class Linked {
+    Node head;
+    Node tail;
     int size;
 
-    Linked(){
-        this.head=null;
-        this.tail=null;
-        this.size=0;
+    Linked() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
     }
 
-    public void addFirst(int val){
+    public void addFirst(int val) {
 
         Node temp = new Node(val);
 
-        if(this.size==0){
-           this.head=this.tail=temp;
-        }else{
-            temp.next=this.head;
-            this.head=temp;
+        if (this.size == 0) {
+            this.head = this.tail = temp;
+        } else {
+            temp.next = this.head;
+            this.head = temp;
         }
 
         this.size++;
     }
 
-    public void display(){
+    public void display() {
         Node temp = this.head;
 
-        while(temp!=null){
+        while (temp != null) {
             System.out.print(temp.data + "  ");
-            temp=temp.next;
+            temp = temp.next;
         }
         System.out.println();
     }
 
-    public void addLast(int val){
+    public void addLast(int val) {
         Node temp = new Node(val);
 
-        if(this.size==0){
-            this.head=this.tail=temp;
-        }else{
-            this.tail.next=temp;
-            this.tail=temp;
+        if (this.size == 0) {
+            this.head = this.tail = temp;
+        } else {
+            this.tail.next = temp;
+            this.tail = temp;
         }
         this.size++;
     }
 
-    public void addAt(int val , int idx){
-        if(idx<0 || idx>size){
+    public void addAt(int val, int idx) {
+        if (idx < 0 || idx > size) {
             System.out.println("InvalidIndex");
 
-        }else if(idx==0){
+        } else if (idx == 0) {
             this.addFirst(val);
-        }else if(idx==this.size){
+        } else if (idx == this.size) {
             this.addLast(val);
-        }else{
+        } else {
             Node temp = new Node(val);
-            Node curr=this.head;
-            while(idx>1){
-                curr=curr.next;
+            Node curr = this.head;
+            while (idx > 1) {
+                curr = curr.next;
                 idx--;
             }
 
             Node nbr = curr.next;
-            curr.next=temp;
-            temp.next=nbr;
+            curr.next = temp;
+            temp.next = nbr;
             this.size++;
 
         }
     }
 
-    public int removeFirst(){
-        if(this.size==0){
+    public int removeFirst() {
+        if (this.size == 0) {
             System.out.println("List is empty");
             return -1;
-        }else{
+        } else if (size == 1) {
             int ele = head.data;
-            this.head=this.head.next;
+            this.head = this.tail = null;
             this.size--;
             return ele;
+
+        } else {
+            int ele = head.data;
+            this.head = this.head.next;
+            this.size--;
+            return ele;
+        }
+    }
+
+    public int removeLast() {
+        if (this.size == 0) {
+            System.out.println("List is empty");
+            return -1;
+        } else if (size == 1) {
+            int ele = head.data;
+            this.head = this.tail = null;
+            this.size--;
+            return ele;
+        } else {
+            Node curr = this.head;
+            for (int i = 0; i <size - 2; i++) {
+                curr = curr.next;
+            }
+            int ele = tail.data;
+            curr.next = null;
+            this.tail = curr;
+            this.size--;
+
+            return ele;
+
         }
     }
 }
