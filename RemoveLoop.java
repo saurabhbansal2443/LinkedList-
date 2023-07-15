583f78ff275a1f5b742a14c27c091983a3ee078a
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-07-15 11:03:57 +0530

    remove loop

--- RemoveLoop.java
+++ RemoveLoop.java
@@ -1,42 +1,138 @@
 public class RemoveLoop {
-    /**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        if(head==null || head.next==null){
-            return null;
+    //{ Driver Code Starts
+// driver code
+
+
+class Node
+{
+    int data;
+    Node next;
+}
+
+class GFG
+{
+    public static Node newNode(int data){
+        Node temp = new Node();
+        temp.data = data;
+        temp.next = null;
+        return temp;
+    }
+    
+    public static void makeLoop(Node head, int x){
+        if (x == 0)
+            return;
+        Node curr = head;
+        Node last = head;
+
+        int currentPosition = 1;
+        while (currentPosition < x)
+        {
+            curr = curr.next;
+            currentPosition++;
         }
-
-        ListNode fast = head;
-        ListNode slow = head ;
-        ListNode ptr = head ;
-
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-
-            if(slow == fast ){
-
-                while(ptr!=slow){
-                    ptr=ptr.next;
-                    slow=slow.next;
-                }
-
-                return ptr ;
-
+        
+        while (last.next != null)
+            last = last.next;
+        last.next = curr;
+    }
+    
+    public static boolean detectLoop(Node head){
+        Node hare = head.next;
+        Node tortoise = head;
+        while( hare != tortoise )
+        {
+            if(hare==null || hare.next==null) return false;
+            hare = hare.next.next;
+            tortoise = tortoise.next;
+        }
+        return true;
+    }
+    
+    public static int length(Node head){
+        int ret=0;
+        while(head!=null)
+        {
+            ret += 1;
+            head = head.next;
+        }
+        return ret;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = newNode(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = newNode(num);
+                tail = tail.next;
             }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, pos);
+            
+            Solution x = new Solution();
+            x.removeLoop(head);
+            
+            if( detectLoop(head) || length(head)!=n )
+                System.out.println("0");
+            else
+                System.out.println("1");
         }
+    }
+}
 
-        return null;
+// } Driver Code Ends
+
+
+/*
+class Node
+{
+    int data;
+    Node next;
+}
+*/
+
+class Solution
+{
+    //Function to remove a loop in the linked list.
+    public static void removeLoop(Node head){
+       if(head==null){
+           return ;
+       }
+       
+       Node fast = head ;
+       Node slow = head ;
+       Node pre = null;
+       Node ptr = head;
+       
+       while(fast!=null && fast.next!=null){
+           pre = slow ;
+           slow=slow.next;
+           fast = fast.next.next;
+           
+           if(fast == slow){
+               
+               while(slow!=ptr){
+                   pre=slow;
+                   slow=slow.next;
+                   ptr=ptr.next;
+               }
+               
+               pre.next=null;
+               return;
+               
+           }
+       }
     }
 }
 }
